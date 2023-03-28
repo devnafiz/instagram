a0375370aa148d556aa667d4587df895e99c51b7
nafiz <nafiz.2011@yahoo.com>
2023-03-28 13:29:50 +0600

    instagram

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,20 @@
+<script setup>
+import MainLayout from '@/Layouts/MainLayout.vue';
+
+</script>
+
+<template>
+    <Head title="Home" />
+    <MainLayout>
+        <div class="bg-green-500">Home Vue</div>
+    </MainLayout>
+
+   
+
+
+  
+</template>
+
+<style>
+
+</style>
